e606a86d622b9eab6a0dc635ccca5de4bce51443
abubakerasifdar <abubakerasifdar100@gmail.com>
2025-07-18 09:52:19 +0500

    Add Some Ui and Event to Check eligibility on one keydown and click as well.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,18 @@
- 
+       document.addEventListener("keydown",(e)=>{
+            if(e.key == "Enter"){
+                myFunction();
+                document.getElementById('age').value = "";
+            }
+        })
         function myFunction() {
             let voteable;
             let age = Number(document.getElementById("age").value);
             if (isNaN(age)) {
                 voteable = "Please enter a valid age";
             } else {
-                voteable = (age < 18) ? "You are not eligible to vote" : "Your are eligible for voting"
+                voteable = (age < 18) ? `You are not eligible to vote ${document.getElementById('output').style.background ="red"}` : `Your are eligible for voting ${document.getElementById('output').style.background ="green"}` 
+                
             };
             document.getElementById("output").style.display = "flex";
-        
             document.getElementById("output").innerHTML = voteable;
         }
